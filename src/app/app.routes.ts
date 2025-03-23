6c45bd2a9022124ede832ea767e4bd427c26b72c
Joaquín Antonio Galdame <92065354+JoaquinGaldame@users.noreply.github.com>
2025-03-23 00:16:48 -0300

    Commit Integration Initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { EpisodesComponent } from './components/pages/episodes/episodes.component';
+import { NotFoundComponent } from './components/pages/notFound/not-found.component';
+import { CharactersListComponent } from './components/pages/characters/characters-list/characters-list.component';
+import { CharactersDetailsComponent } from './components/pages/characters/characters-details/characters-details.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'characters-list', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'episodes', component: EpisodesComponent },
+    { path: 'characters-list', component: CharactersListComponent },
+    { path: 'characters-details/:id', component: CharactersDetailsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: NotFoundComponent },
+];
+
---
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-card',
+  imports: [],
+  templateUrl: './characters-card.component.html',
+  styleUrl: './characters-card.component.css'
+})
+export class CharactersCardComponent {
+
+}
